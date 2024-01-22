8f90b1413e2b4b778c0157906a5492f48f5e88db
Nikolai Ekkore <nikolai_ekkore@epam.com>
2024-01-23 03:09:20 +0600

    initial commit

---
+++ lib/checkers.ex
@@ -0,0 +1,18 @@
+defmodule Checkers do
+  @moduledoc """
+  Documentation for `Checkers`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Checkers.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/checkers_test.exs
@@ -0,0 +1,8 @@
+defmodule CheckersTest do
+  use ExUnit.Case
+  doctest Checkers
+
+  test "greets the world" do
+    assert Checkers.hello() == :world
+  end
+end
